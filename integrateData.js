65bd29af6969dc86271a83227ad8e8373a5134eb
Phillip Dickey <58786787+phillipjad@users.noreply.github.com>
2023-10-17 22:42:48 -0400

    Update integrateData.js

--- integrateData.js
+++ integrateData.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 /**
 * This function syncs data to cosmosDB
-* @param data - the data to be pushed to cosmosDB
-* @return int - a 1 if successful and a 0 if not
+* @param {object} data - the data to be pushed to cosmosDB
+* @return {number} - a 1 if successful and a 0 if not
 */
 async function integrateData(data){
   var returnid = 0, apiurl = ""; //apiurl will be Azure Function endpoint
