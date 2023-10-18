7d7db4c2cb487790eb08b28cd59b2a53b47a9c62
Phillip Dickey <58786787+phillipjad@users.noreply.github.com>
2023-10-17 22:45:04 -0400

    Update fetchInternalActivity.js

--- fetchInternalActivity.js
+++ fetchInternalActivity.js
@@ -1,12 +1,15 @@
 const axios = require('axios');
 
-
-//This function will call an Azure Function that will
-//access an Azure CosmosDB instance and return matching
-//activities within it
-
-//Takes an activityId and returns an array of activities upon
-//successful retrieval or an empty array otherwise
+/**
+*This function will call an Azure Function that will
+*access an Azure CosmosDB instance and return matching
+*activities within it
+*
+*Takes an activityId and returns an array of activities upon
+*successful retrieval or an empty array otherwise
+* @param {number} activityId - number representing the id of an activity to be fetched for
+* @return {array} - an array either full of matching activites or empty
+*/
 async function fetchInternalActivity(activityId){
   var internalActivities;
   var apiurl = ""; //Will eventually be set to the url endpoint of our Azure Function
