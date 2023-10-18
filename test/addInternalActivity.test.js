6aaf0aa1a58b259feb13e351296dec89d03bebf1
Phillip Dickey <58786787+phillipjad@users.noreply.github.com>
2023-10-17 22:58:14 -0400

    Create addInternalActivity.test.js

---
+++ test/addInternalActivity.test.js
@@ -0,0 +1,14 @@
+const addInternalActivity = require('./addInternalActivity')
+
+test('Adds a new activity to the internal database.' async() =>{
+  const mockCreateActivity = jest.fn((data) => {
+    return {
+      id: 'uniqueActivityID',	//add more properties
+    };
+  });
+  
+  const createdActivity = addInternalActivity(activityData)
+  expect(createdActivity).toEqual({
+    id: 'uniqueActivityID', // add more property checks if applicable
+  });
+});
