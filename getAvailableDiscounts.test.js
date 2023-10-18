1840344aeefd45ec49c13d0230785e5b91902a89
Phillip Dickey <58786787+phillipjad@users.noreply.github.com>
2023-10-17 22:57:19 -0400

    Delete getAvailableDiscounts.test.js

--- getAvailableDiscounts.test.js
+++
@@ -1,8 +0,0 @@
-const getAvailableDiscounts = require('./p2maybecode')
-
-
-test('prints a list of discounts from the api', async() =>{
-	const title = await getAvailableDiscounts();
-	expect(title).toEqual('insert title here'); //insert a title from the api here
-	});
-	
