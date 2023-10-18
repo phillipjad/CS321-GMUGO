ca50656a4805dd084184b632e66182d11327fd8e
Srefai <113073913+Srefai@users.noreply.github.com>
2023-10-17 23:20:52 -0400

    Update integrateData.test.js

--- test/integrateData.test.js
+++ test/integrateData.test.js
@@ -1,7 +1,7 @@
 const integrateData = require('./p2maybecode')
 const data;  //add on to get data
 
-test('Integrates and merges data from both internal and external sources for a unifi    ed response.', async() =>{
+test('Integrates and merges data from both internal and external sources for a unified response.', async() =>{
   const retid = integrateData(data);
   expect(retid).toBeInstanceOf(int);
   expect(retid === 0 || retid === 1).toBe(true);
