d84db049517a0510d6c8dee225af9152227905bd
Srefai <113073913+Srefai@users.noreply.github.com>
2023-10-17 23:11:16 -0400

    Update integrateData.test.js
    Typo update

--- test/integrateData.test.js
+++ test/integrateData.test.js
@@ -4,5 +4,5 @@ const data;  //add on to get data
 test('Integrates and merges data from both internal and external sources for a unifi    ed response.', async() =>{
   const retid = integrateData(data);
   expect(retid).toBeInstanceOf(int);
-  expect(retid === 0 || rtid === 1).toBe(true);
+  expect(retid === 0 || retid === 1).toBe(true);
 });
