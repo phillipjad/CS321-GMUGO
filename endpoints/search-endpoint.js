f0dc90cde307f352ac79c40165b93805ac3fc9b3
Srefai <113073913+Srefai@users.noreply.github.com>
2023-12-04 13:33:08 -0500

    Update search-endpoint.js

--- endpoints/search-endpoint.js
+++ endpoints/search-endpoint.js
@@ -6,7 +6,3 @@ exports.search = (req, res) => {
   const results = searchData.filter(item => item.name.toLowerCase().includes(query.toLowerCase()));
   res.json(results);
 };
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
